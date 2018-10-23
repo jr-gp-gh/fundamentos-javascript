1e729ff237c0caafd9e176e01495d2ba8ee9e2f1
jr-gp-gh <jr.gammparters@outlook.com>
2018-10-23 10:40:35 -0600

    Strings

---
+++ clase2.js
@@ -0,0 +1,17 @@
+var nombre = 'Sacha', apellido = 'Lifszyc'
+
+var nombreEnMayusculas = nombre.toUpperCase()
+
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+var cantidadDeLetrasDelNombre = nombre.length
+
+// var nombreCompeto = nombre + ' ' + apellido
+
+var nombreCompeto = `${nombre} ${apellido.toUpperCase()}`
+
+var str = nombre.substr(1, 2)
+
+var ultimaLetraDelNombre = nombre.charAt(nombre.length-1)
+// var lastChar = name.slice(-1)
