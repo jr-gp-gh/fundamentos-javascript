bc2ec75de9403fb4c2f19c13da1c1bd1b56f11eb
jr-gp-gh <jr.gammparters@outlook.com>
2018-10-23 10:39:07 -0600

    variables

---
+++ clase1.js
@@ -0,0 +1,10 @@
+var nombre = 'Yo', apellido = 'yo'
+var edad = 28
+
+edad = '28 años'
+
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('Tengo ' + edad)
+
+var peso = 75
+console.log('Peso ' + peso)
