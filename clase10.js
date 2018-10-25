1a9c612463d28e18731047e5ca62ff718823b83a
jr-gp-gh <jr.gammparters@outlook.com>
2018-10-25 11:49:27 -0600

    Condicionales

---
+++ clase10.js
@@ -0,0 +1,37 @@
+var sacha = {
+    nombre: 'Sacha',
+    apellido: 'Lifszync',
+    edad: 28,
+    ingeniero: true,
+    cocinero: false,
+    cantante: true,
+    dj: false,
+    guitarrista: false,
+    drone: true
+}
+
+
+function imprimirProfesiones(persona) {
+    console.log(`${persona.nombre} es:`)
+    
+    if (persona.ingeniero) {
+        console.log(`Ingeniero`)
+    }
+    if (persona.cocinero) {
+        console.log(`Cocinero`)
+    }
+    if (persona.cantante) {
+        console.log(`Cantante`)
+    }
+    if (persona.dj) {
+        console.log(`Dj`)
+    }
+    if (persona.guitarrista) {
+        console.log(`Guitarrista`)
+    }
+    if (persona.drone) {
+        console.log(`Drone`)
+    }
+}
+
+imprimirProfesiones(sacha)
