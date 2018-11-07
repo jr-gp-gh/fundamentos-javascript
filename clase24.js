fe4ee0e01556aa5055d3ecf5007268d8bbd88c7d
jr-gp-gh <jr.gammparters@outlook.com>
2018-11-06 17:32:57 -0700

    Clases Verdad Oculta

---
+++ clase24.js
@@ -0,0 +1,39 @@
+function heredaDe(prototipoHijo, prototipoPadre) {
+    var fn = function () {}
+    fn.prototype = prototipoPadre.prototype
+    prototipoHijo.prototype = new fn
+    prototipoHijo.prototype.constructor = prototipoHijo  
+}
+
+function Persona(nombre, apellido, altura) {
+    this.nombre = nombre
+    this.apellido = apellido
+    this.altura = altura
+}
+
+Persona.prototype.saludar = function() {
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+}
+
+Persona.prototype.soyAlto = function() {
+    // debugger   
+    return this.altura >= 1.8
+}
+
+function Desarrollador(nombre, apellido) {
+    this.nombre = nombre
+    this.apellido = apellido
+}
+
+heredaDe(Desarrollador, Persona)
+
+Desarrollador.prototype.saludar = function () {
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador/a`)
+}
+
+var sacha = new Persona('Sacha', 'Lifszync', 1.80)
+var erika = new Persona('Erika', 'Luna', 1.65)
+var arturo = new Desarrollador('Arturo', 'Martinez', 1.89)
+
+sacha.saludar()
+arturo.saludar()
