23e712e685388581621080cc6e21b95537723b7e
Development <development@ACC8C86A.ipt.aol.com>
2018-11-26 23:40:15 -0700

    Asincronimso

---
+++ clase35.js
@@ -0,0 +1,35 @@
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+
+const opts = { crossDomain: true }
+
+function obtenerPersonaje(id) {
+    return new Promise((resolve, reject) => {
+        const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+        $
+        .get(url, opts, function (data){
+            resolve(data) //resolver la promesa y no sel lama hasta que el get sea exitoso
+        })
+        .fail(() => reject(id))
+    })
+}
+
+function onError(id) {
+    console.log(`Sucedió un error al obtener el personaje ${id}`)
+}
+
+async function obtenerPersonajes() {
+    var ids = [1, 2, 3, 4, 5, 6]
+    var promesas = ids.map(id => obtenerPersonaje(id))
+    try {
+        var personajes = await Promise.all(promesas)
+    } catch (id) {
+        onError(id)
+    }
+    Promise
+        .all(promesas)
+        .then(personajes => console.log(personajes))
+        .catch(onError)
+}
+
+obtenerPersonajes()
